1a03724c893e618b4a21b14b99cd7921c69dbf50
albamolera <albamolerachacon@gmail.com>
2022-06-16 13:32:28 +0200

    feat: mejora estilos home

--- components/HomeSearcher.tsx
+++ components/HomeSearcher.tsx
@@ -1,59 +1,61 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-import styles from '../styles/Searcher.module.css'
-import Link from 'next/link'
+import styles from "../styles/Searcher.module.css";
+import Link from "next/link";
 
 export const HomeSearcher = () => {
+  const router = useRouter();
+  const [oferta, setOferta] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push("/ofertas/?query=" + oferta);
+  };
 
-    const router = useRouter()
-    const [oferta, setOferta] = useState('');
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        router.push('/ofertas/?query=' + oferta)
-    }
-
-    return (
-        <div className={styles.searcher}>
-            <h1>
-                <span>Ya era hora</span> 
-                <br/>
-                <span>Solo las mejores ofertas</span>
-            </h1>
-            <div className={styles.searcherForm}>
-              <form method="post" onSubmit={handleSubmit}>
-                  <input 
-                      id="oferta"
-                      name="oferta"
-                      type="text"
-                      onChange={e => setOferta(e.target.value)}
-                      value={oferta}
-                      placeholder="Busca en Open Jobs"
-                  />
-                  <button type="submit" className={styles.searcherHomeButton}>Buscar</button>
-              </form>
-            </div>
-            <div className={styles.searcherHomeFilters}>
-              <Link href="#">
-                <a>Javascript</a>
-              </Link>
-              <Link href="#">
-                <a>Madrid</a>
-              </Link>
-              <Link href="#">
-                <a>Python</a>
-              </Link>
-              <Link href="#">
-                <a>Front-end</a>
-              </Link>
-              <Link href="#">
-                <a>Node.js</a>
-              </Link>
-              <Link href="#">
-                <a>Remoto</a>
-              </Link>
-            </div>
+  return (
+    <div className={styles.searcher}>
+      <h1>
+        <span>Ya era hora</span>
+        <br />
+        <span>Solo las mejores ofertas</span>
+      </h1>
+      <div className={styles.searcherForm}>
+        <form method="post" onSubmit={handleSubmit}>
+          <div>
+            <input
+              id="oferta"
+              name="oferta"
+              type="text"
+              onChange={(e) => setOferta(e.target.value)}
+              value={oferta}
+              placeholder="Busca en Open Jobs"
+            />
+            <button type="submit" className={styles.searcherHomeButton}>
+              Buscar
+            </button>
+          </div>
+        </form>
+        <div className={styles.searcherHomeFilters}>
+          <Link href="#">
+            <a>Javascript</a>
+          </Link>
+          <Link href="#">
+            <a>Madrid</a>
+          </Link>
+          <Link href="#">
+            <a>Python</a>
+          </Link>
+          <Link href="#">
+            <a>Front-end</a>
+          </Link>
+          <Link href="#">
+            <a>Node.js</a>
+          </Link>
+          <Link href="#">
+            <a>Remoto</a>
+          </Link>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
