b67987b4fbd3f5c7716daf24e8ab0a1ee2aa7135
AdrianBlancoGarcia <adrian@open-bootcamp.com>
2022-06-20 13:46:53 +0200

    feat: reparaciones e implementacion

---
+++ pages/hazte-open-bootcamper.tsx
@@ -0,0 +1,13 @@
+import HeroOpenBootcamper from '../components/bootcamper/Hero'
+import { Navbar } from '../components/navBar'
+
+const BeOpenBootcamper = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroOpenBootcamper />
+    </>
+  )
+}
+
+export default BeOpenBootcamper
