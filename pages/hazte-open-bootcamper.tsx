40389f35b37af4094102b1f7f3a04ffedfd23344
Cesk <ceskk97@gmail.com>
2022-06-20 16:45:45 +0200

    Feat: Funcionalidad testimonios añadida con json de prueba

--- pages/hazte-open-bootcamper.tsx
+++ pages/hazte-open-bootcamper.tsx
@@ -1,11 +1,16 @@
+import { GetStaticProps } from 'next'
 import HeroOpenBootcamper from '../components/bootcamper/Hero'
+import { Testimonios } from '../components/hazte_open_bootcamper/Testimonios'
 import { Navbar } from '../components/navBar'
+
+import testimonios from '../utils/data/testimonios.json'
 
 const BeOpenBootcamper = () => {
   return (
     <>
       <Navbar />
       <HeroOpenBootcamper />
+      <Testimonios testimonios={testimonios} />
     </>
   )
 }
