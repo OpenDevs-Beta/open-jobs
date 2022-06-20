65be9e5f3073e0c71444e4b3db2ec988cb9f0719
albamolera <albamolerachacon@gmail.com>
2022-06-20 17:01:09 +0200

    feat: accordeon hazte-open-bootcamper

--- pages/hazte-open-bootcamper.tsx
+++ pages/hazte-open-bootcamper.tsx
@@ -1,13 +1,34 @@
-import HeroOpenBootcamper from '../components/bootcamper/Hero'
-import { Navbar } from '../components/navBar'
+import HeroOpenBootcamper from "../components/bootcamper/Hero";
+import Accordeon from "../components/hazte_open_bootcamper/Accordeon";
+import { Navbar } from "../components/navBar";
 
 const BeOpenBootcamper = () => {
+  const questions = [
+    {
+      question: "Soy autónomo, ¿Puedo facturar la recompensa?",
+      answer: "Respuesta 1",
+    },
+    {
+      question: "¿Qué pasa si no quiero recibir la recompensa?",
+      answer: "respuesta 2",
+    },
+    {
+      question: "¿Qué pasa si no quiero recibir la recompensa?",
+      answer: "respuesta 2",
+    },
+    {
+      question: "¿Qué pasa si no quiero recibir la recompensa?",
+      answer: "respuesta 2",
+    },
+  ];
+
   return (
     <>
       <Navbar />
       <HeroOpenBootcamper />
+      <Accordeon items={questions}></Accordeon>
     </>
-  )
-}
+  );
+};
 
-export default BeOpenBootcamper
+export default BeOpenBootcamper;
