67a8c549d678f2054665715d67ea0302f769eddf
Cesk <ceskk97@gmail.com>
2022-06-22 16:23:48 +0200

    Feat: Filters styles

--- components/filtros/FilterCiudad.tsx
+++ components/filtros/FilterCiudad.tsx
@@ -7,26 +7,51 @@ const FilterCiudad = (query: any) => {
   const router: any = useRouter()
 
   const handleClick = (filter: string) => {
-    router.replace("/ofertas/?ciudad=" + filter)
+    router.query.ciudad != filter ? router.query.ciudad = filter : router.query.ciudad = ''
+    router.push({ 
+      pathname: '/ofertas',
+      query: router.query
+    })
   }
 
+  // TODO: Modificar cuando se tenga la api del back
   return (
-    <div className={styles.filterCiudad}>
-      <input placeholder="Madrid..." onChange={(e) => handleClick(e.target.value)} />
-      <div>
-        <span onClick={() => handleClick('madrid')}>Madrid</span>
+    <div className={styles.filter}>
+      <input type="text" placeholder="Madrid..." onChange={(e) => handleClick(e.target.value)} />
+      <div onClick={() => handleClick('madrid')}>
+        {  router.query.ciudad == 'madrid' ? 
+          (<span className={styles.selected}>Madrid</span>)
+        :
+          (<span>Madrid</span>)
+        }
       </div>
-      <div>
-        <span onClick={() => handleClick('barcelona')}>Barcelona</span>
+      <div onClick={() => handleClick('barcelona')}>
+        {  router.query.ciudad == 'barcelona' ? 
+          (<span className={styles.selected}>Barcelona</span>)
+        :
+          (<span>Barcelona</span>)
+        }
       </div>
-      <div>
-        <span onClick={() => handleClick('malaga')}>Málaga</span>
+      <div onClick={() => handleClick('malaga')}>
+        {  router.query.ciudad == 'malaga' ? 
+          (<span className={styles.selected}>Málaga</span>)
+        :
+          (<span>Málaga</span>)
+        }
       </div>
-      <div>
-        <span onClick={() => handleClick('valencia')}>Valencia</span>
+      <div onClick={() => handleClick('valencia')}>
+        {  router.query.ciudad == 'valencia' ? 
+          (<span className={styles.selected}>Valencia</span>)
+        :
+          (<span>Valencia</span>)
+        }
       </div>
-      <div>
-        <span onClick={() => handleClick('alicante')}>Alicante</span>
+      <div onClick={() => handleClick('alicante')}>
+        {  router.query.ciudad == 'alicante' ? 
+          (<span className={styles.selected}>Alicante</span>)
+        :
+          (<span>Alicante</span>)
+        }
       </div>
     </div>
   )
