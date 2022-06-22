3f68e100cc4532e976c299aacd761cac02b3a218
albamolera <albamolerachacon@gmail.com>
2022-06-22 16:28:51 +0200

    feat: query params middlewares

--- shared/middlewares/APImiddleware.ts
+++ shared/middlewares/APImiddleware.ts
@@ -1,50 +1,77 @@
-import fetch from 'isomorphic-unfetch'
+import fetch from "isomorphic-unfetch";
 
-const ENDPOINT = '/api/ofertas'
+const ENDPOINT = "/api/ofertas";
 
 /**
  * Method to get all jobs
  * @returns Json data and isError
  */
-export async function getAllJobs() {
-  
-  let response = await fetch(process.env.NEXT_PUBLIC_API_URL + ENDPOINT)
-  .then(r => r.json())
-  .catch(e => console.error(`Error al hacer petición de ofertas: ${e}`))
+export async function getAllJobs({
+  sortBy,
+  sortType,
+}: {
+  sortBy?: string;
+  sortType?: string;
+} = {}) {
+  let query = [];
+
+  if (sortBy) {
+    query.push(`sortBy=${sortBy}`);
+  }
+
+  if (sortType) {
+    query.push(`sortType=${sortType}`);
+  }
+
+  let url = process.env.NEXT_PUBLIC_API_URL + ENDPOINT;
+
+  if (query.length > 0) {
+    url += `?${query.join("&")}`;
+  }
+
+  debugger;
+
+  console.log(url);
+
+  let response = await fetch(url)
+    .then((r) => r.json())
+    .catch((e) => console.error(`Error al hacer petición de ofertas: ${e}`));
 
   return {
     data: response?.data?.data,
-  }  
+  };
 }
 
-/** 
+/**
  * Method to get jobs by page
- * @param page 
+ * @param page
  * @returns Json data and isError
  */
- export async function getJobsPaginated(page: any) {
-  
-  let response = await fetch(process.env.NEXT_PUBLIC_API_URL + ENDPOINT + `/?page=${page}`)
-  .then(r => r.json())
-  .catch(e => console.error(`Error al hacer petición de ofertas: ${e}`))
+export async function getJobsPaginated(page: any) {
+  let response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + ENDPOINT + `/?page=${page}`
+  )
+    .then((r) => r.json())
+    .catch((e) => console.error(`Error al hacer petición de ofertas: ${e}`));
 
   return {
     data: response?.data?.data,
-  }  
+  };
 }
 
 /*
  * Method to get job by Id
- * @param id 
+ * @param id
  * @returns Json data and isError
  */
 export async function getJobById(id: any) {
-
-	let response = await fetch(process.env.NEXT_PUBLIC_API_URL + ENDPOINT + `/${id}`)
-		.then(r => r.json())
-    .catch(e => console.error(`Error al hacer petición de ofertaID: ${e}`))
+  let response = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + ENDPOINT + `/${id}`
+  )
+    .then((r) => r.json())
+    .catch((e) => console.error(`Error al hacer petición de ofertaID: ${e}`));
 
   return {
     data: response?.data,
-  }  
+  };
 }
