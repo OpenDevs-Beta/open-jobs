68ec86b1bf300ba4454be5bbf94dd5dcc142ccb9
bradley-us <bradleydev101@gmail.com>
2022-06-25 17:14:05 +0100

    Faqs controlled by btn

--- pages/preguntas-frecuentes.tsx
+++ pages/preguntas-frecuentes.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 import Questions from "../components/questions";
 import QuestionSearcher from "../components/questionsSearcher";
 import styles from "../styles/index.module.css";
@@ -13,11 +12,22 @@ export default function preguntasFrecuentes() {
 
   const queryParam = router.query.query;
 
-  const [questionsSearch, setQuestionsSearch] = useState<Object[]>(questions);
+  const [block, setBlock] = useState(10)
+  
+  const questionsBlock = questions.slice(0, block)
+
+  console.log(questionsBlock.length)
+
+  const [questionsSearch, setQuestionsSearch] = useState<Object[]>(questionsBlock);
+
+  const loadMoreQuestions = () => {
+    setBlock(prevState => prevState +10)
+    setQuestionsSearch(questionsBlock)
+  }
 
   const search = () => {
     if (queryParam === undefined || queryParam === "") {
-      setQuestionsSearch(questions);
+      setQuestionsSearch(questionsBlock);
     } else {
       let dataChange = questions.filter((question: any) => {
         // .normalize("NFD").replace(/[\u0300-\u036f]/g, "") Quitar tildes //
@@ -43,12 +53,21 @@ export default function preguntasFrecuentes() {
 
   useEffect(() => {
     search();
-  }, [queryParam]);
+  }, [queryParam, block]);
 
   return (
     <div className={styles.homepageContainer}>
       <QuestionSearcher/>
       <Questions questions={questionsSearch} />
+      <div className={styles.btnLoadMoreQuestionsContainer}>
+        {
+          questions.length > block ? (
+            <button className={styles.btnMoreQuestions} onClick={loadMoreQuestions}>Más preguntas</button>
+          ): (
+            <p className={styles.textNoQuestions}>No hay más preguntas</p>
+          )
+        }
+      </div>
       <Footer />
     </div>
   );
